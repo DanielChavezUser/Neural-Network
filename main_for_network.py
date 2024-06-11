bc0f6193ca7acba685f55fd45252121fe76610a7
DanielChavezUser <123327675+DanielChavezUser@users.noreply.github.com>
2024-06-10 19:56:24 -0500

    Update main_for_network.py

--- main_for_network.py
+++ main_for_network.py
@@ -1,16 +1,20 @@
-# -*- coding: utf-8 -*-
 """
 Created on Tue Jun  4 13:18:46 2024
 
 @author: danch
 """
-import mnist_loader
-import network
+import mnist_loader1
+import network1
 
 def main():
-    print("insdide")
-    nn = network([784, 40, 10])
-    training_data, testing_data = mnist_loader.load_data_wrapper()
     
-    nn.SGD(training_data, 30, .3, testing_data)
+    nn = network1.Network([784, 40, 10])
+    training_data, testing_data = mnist_loader1.load_data_wrapper()
+    
+    nn.SGD(training_data, 30, 10, 3.0, testing_data)
     return 0
+
+if __name__ == "__main__":
+    main()
+    
+    
